ad1355bdc1bc2d77e1b84b6cdc403d57b1e5c68d
sora1129 <yuka.izumisawa@sweeep.ai>
2022-09-04 10:35:01 +0900

    feat: completion

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-'use strict'
+'use strict';
 
- {
+{
   const btn = document.getElementById('btn');
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '凶', '末吉'];
-    const n = Math.floor(Math.random() * results.length);
-    btn.textContent = results[n];
+    const resuluts = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * resuluts.length);
+    btn.textContent = resuluts[n];
   });
-
- }
+}
